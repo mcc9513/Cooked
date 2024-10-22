7139fe471ec5d5a1150ac59ecd6cb6bee95f79d8
mcc9513 <58860627+mcc9513@users.noreply.github.com>
2024-10-22 15:09:57 -0400

    Delete tableexample.sql

--- tableexample.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE recipe_files (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    recipe_id INT,
-    file_type ENUM('image', 'instruction', 'other'),
-    bucket_name VARCHAR(255),
-    file_key VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (recipe_id) REFERENCES recipes(id)
-);
